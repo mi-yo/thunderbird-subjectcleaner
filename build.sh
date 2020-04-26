c73b621ca8d9a3b8f98ca1fa3cba66ff1bd7150b
mi-yo <miyamoto.yoichi@gmail.com>
2020-04-26 14:16:18 +0900

    Update build.sh. (inspired by whistler-mb)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-VERSION=`grep em:version install.rdf | tr -d "[:space:]<em:version>/"`
+VERSION=`grep '"version":' manifest.json | tr -d '[:space:],:"version'`
 zip -r subject_cleaner-$VERSION-tb.xpi content defaults locale chrome.manifest install.rdf manifest.json
